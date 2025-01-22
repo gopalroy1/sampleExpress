218abde978b62791ca12ee69fb3d35dc81c63a97
Gopal Roy <105766337+gopalroy1@users.noreply.github.com>
2025-01-22 15:17:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 
 
   app.get('/hello', async (req, res) => {
+    console.log('req came')
     try {
       const response = await axios.get('https://smartzone.reliancegeneral.co.in/Login/IMDLogin?ReturnUrl=%2fMotor%2fPartialQuoteProducTypes%3fbaseProductCode%3dqNqXB5%25252bwj5cfBNx%25252fo5dSgA%25253d%25253d&baseProductCode=qNqXB5%252bwj5cfBNx%252fo5dSgA%253d%253d');
       console.log('sucess')
@@ -18,5 +19,5 @@
   });
 
   app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port P: ${port}`);
   });
