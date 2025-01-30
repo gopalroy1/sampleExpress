f81b27b19b15a04a7dee9111b6b775b94968c942
Gopal Roy <105766337+gopalroy1@users.noreply.github.com>
2025-01-30 12:33:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,8 @@
   app.get('/hello', async (req, res) => {
     try {
       console.log('req came')
+      const res1 = await axios.post('https://x3zupunkzxixtdf3rigvonm3jq0sinpv.lambda-url.ap-south-1.on.aws/');
+      console.log('res1 complete',{res1});
       const response = await axios.get('https://smartzone.reliancegeneral.co.in/Login/IMDLogin?ReturnUrl=%2fMotor%2fPartialQuoteProducTypes%3fbaseProductCode%3dqNqXB5%25252bwj5cfBNx%25252fo5dSgA%25253d%25253d&baseProductCode=qNqXB5%252bwj5cfBNx%252fo5dSgA%253d%253d');
       console.log('sucess')
       res.send('sucess');
