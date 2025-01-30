91364747aa2d81010c8c4aed3daef7b8c720368b
Gopal Roy <105766337+gopalroy1@users.noreply.github.com>
2025-01-30 13:49:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,18 +13,18 @@
       console.log('res1 complete',{res1});
       const response = await axios.get('https://smartzone.reliancegeneral.co.in/Login/', null, {
             headers: {
-                // 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
-                // 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-                // 'Accept-Language': 'en-US,en;q=0.9',
-                // 'Accept-Encoding': 'gzip, deflate, br',
-                // 'Referer': 'https://target-website.com/',
-                // 'DNT': '1', // Prevents tracking suspicion
-                // 'Upgrade-Insecure-Requests': '1',
-                // 'Connection': 'keep-alive',
-                // 'Cache-Control': 'no-cache',
-                // 'X-Requested-With': 'XMLHttpRequest',
-                // 'X-Forwarded-For': '45.67.89.101', // Fake IP (optional)
-                // 'Origin': 'https://target-website.com'
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+                'Accept-Language': 'en-US,en;q=0.9',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Referer': 'https://target-website.com/',
+                'DNT': '1', // Prevents tracking suspicion
+                'Upgrade-Insecure-Requests': '1',
+                'Connection': 'keep-alive',
+                'Cache-Control': 'no-cache',
+                'X-Requested-With': 'XMLHttpRequest',
+                'X-Forwarded-For': '45.67.89.101', // Fake IP (optional)
+                'Origin': 'https://target-website.com'
             }
         });
       console.log('all completed')
