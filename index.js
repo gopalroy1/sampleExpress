c761876d3f42ce8fb2df211237a2a7ca8d2a0b49
gopalaegis <gopal@aegiscovenant.com>
2025-03-21 19:19:48 +0530

    fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3002;
 
 app.get('/hello', async (req, res) => {
-  console.log('Code updated')
+  console.log('Code updated 2')
   try {
     return res.status(200).json('Hey there! code updated')
   } catch (error) {
