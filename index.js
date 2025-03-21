01fc013d3e91e5f455cc8b2b466acfdc4816e7b5
gopalaegis <gopal@aegiscovenant.com>
2025-03-21 17:34:31 +0530

    console added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 3002;
 
 app.get('/hello', async (req, res) => {
+  console.log('Req came')
   try {
     return res.status(200).json('Hey there!')
   } catch (error) {
